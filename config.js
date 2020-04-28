71ee3bd91f01564979b9cdda676a0e93a0cd4b11
software2015 <hhll69241@gmail.com>
2020-04-28 14:03:08 +0300

    Adding production configuration

--- config.js
+++ config.js
@@ -3,6 +3,10 @@ require('dotenv').config();
 var { Pool } = require('pg');
 var isProduction = process.env.NODE_ENV === 'production'
 
+console.log("////////////////////////////////////////////////////")
+console.log(isProduction)
+console.log("////////////////////////////////////////////////////")
+
 var connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`
 
 var pool = new Pool({
