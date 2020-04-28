1095638d3df1dc9486a9a8ff58f2e446f5733429
software2015 <hhll69241@gmail.com>
2020-04-28 13:53:12 +0300

    Adding production configuration

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ var isProduction = process.env.NODE_ENV === 'production'
 var connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`
 
 var pool = new Pool({
-	connectionString: isProduction ? process.env.DATABASE_URL + "?ssl=true" : connectionString,
+	connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
 	ssl: isProduction,
 })
 
